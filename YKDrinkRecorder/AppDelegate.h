76ec213a7740a15af0d005fe63e8f8db0f5f9ca2
Kboy <yankang_hpu@163.com>
2018-05-27 09:13:59 +0800

    Initial Commit

---
+++ YKDrinkRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YKDrinkRecorder
+//
+//  Created by yankang on 2018/5/27.
+//  Copyright © 2018年 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YKDrinkRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YKDrinkRecorder
+//
+//  Created by yankang on 2018/5/27.
+//  Copyright © 2018年 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
